8daae8637ba3b11238d6c8686a36178e5c5b5e5c
unknown <oleti.manikanta@gmail.com>
2022-12-28 15:07:29 +0530

    initial project setup

---
+++ src/main/java/com/myIT/SpringSecurity/Controllers/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.myIT.SpringSecurity.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Hi Welcome to Spring Security Course";
+    }
+}
